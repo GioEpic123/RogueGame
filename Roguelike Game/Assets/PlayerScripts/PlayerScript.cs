31a7ed5d4282f25a1e63bb6158beed757fbeba87
Giovanni Quevedo <giovanni.quevedo5@gmail.com>
2022-09-06 18:08:57 -0700

    Adding code to repo

---
+++ Roguelike Game/Assets/PlayerScripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour, iDamagable<float>
+{
+
+    //Player Script to handle HP, Inventory, Fire Mode, ETC
+    public float health = 100f;
+    public GameObject gameManager;
+    public Transform playerTrans;
+    public GameObject currentRoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Keeps the Player from flipping over and flying away
+        playerTrans.rotation.Set(0, playerTrans.rotation.y, 0,playerTrans.rotation.w);
+    }
+
+    public void takeDamage(float damageTaken)
+    {
+        if (health > damageTaken)
+        {
+            health -= damageTaken;
+            //Update HUD health
+        }  
+        else
+            gameManager.GetComponent<GameManager>().gameOver();
+    }
+
+    public void setRoom(GameObject room)
+    {
+        if (currentRoom != room)
+            currentRoom = room;
+    }
+}
